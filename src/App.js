516cad90fcb58bdbbb67de1e36b9aa77df6e901a
whimchang <whimchang@163.com>
2017-06-28 11:17:15 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+/*class App extends Component {
   render() {
     return (
       <div className="App">
@@ -16,125 +16,113 @@ class App extends Component {
       </div>
     );
   }
+}*/
+
+function filterList(list, search) {
+    var realList = [];
+    list.forEach((item)=>{
+        if(!search){
+            realList.push(item);
+        }else if(item.name.indexOf(search.name)>=0){
+            realList.push(item);
+        }
+    });
+    return realList;
 }
 
-function getList() {
-    return {
-        data: [
-            {
-                name: 'John1',
-                age: 37,
-                grade: 1
-            },
-            {
-                name: 'John2',
-                age: 19,
-                grade: 2
-            },
-            {
-                name: 'John3',
-                age: 25,
-                grade: 3
-            },
-            {
-                name: 'John4',
-                age: 11,
-                grade: 4
-            },
-            {
-                name: 'John5',
-                age: 44,
-                grade: 5
-            },
-            {
-                name: 'John6',
-                age: 27,
-                grade: 6
-            },
-            {
-                name: 'John7',
-                age: 29,
-                grade: 7
-            },
-            {
-                name: 'John8',
-                age: 41,
-                grade: 4
-            }
-         ]
-    }
-}
-
-function pagination(list,index,per) {
+function pagination(list,index,per,handle) {
     var rows = [];
-    list.map((item,index_item)=>{
+    list.forEach((item,index_item)=>{
         if(index_item >= (index-1)*per && index_item<index*per){
-            rows.push(<ResultItem item={item} index={index_item} key={item.name}></ResultItem>);
+            rows.push(<ResultItem item={item} index={index_item} key={item.name} onHandleItemDelete={handle}></ResultItem>);
         }
     });
     return rows;
 }
 
 class Search extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    handleSearchName = (e)=>{
+        this.props.onHandleSearchName(e.target.value);
+    };
+
     render(){
         return (
             <div className="Search">
                 <label>姓名</label>&nbsp;&nbsp;
-                <input type="text" value={this.props.search.name}/>&nbsp;&nbsp;&nbsp;&nbsp;
-                <label>年龄</label>&nbsp;&nbsp;
-                <input type="number" value={this.props.search.age}/>&nbsp;&nbsp;&nbsp;&nbsp;
-                <label>年级</label>&nbsp;&nbsp;
-                <select name="grade" id="grade" value={this.props.search.grade}>
-                    <option value="null">请选择</option>
-                    <option value="1">Grade 1</option>
-                    <option value="2">Grade 2</option>
-                    <option value="3">Grade 3</option>
-                    <option value="4">Grade 4</option>
-                </select>&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="text"
+                       value={this.props.search.name}
+                       onChange={this.handleSearchName}/>&nbsp;&nbsp;&nbsp;&nbsp;
+                {/*<label>年龄</label>&nbsp;&nbsp;*/}
+                {/*<input type="number" value={this.props.search.age}/>&nbsp;&nbsp;&nbsp;&nbsp;*/}
+                {/*<label>年级</label>&nbsp;&nbsp;*/}
+                {/*<select name="grade" id="grade" value={this.props.search.grade}>*/}
+                    {/*<option value="null">请选择</option>*/}
+                    {/*<option value="1">Grade 1</option>*/}
+                    {/*<option value="2">Grade 2</option>*/}
+                    {/*<option value="3">Grade 3</option>*/}
+                    {/*<option value="4">Grade 4</option>*/}
+                {/*</select>&nbsp;&nbsp;&nbsp;&nbsp;*/}
             </div>
         );
     }
 }
 
+
 class Pagination extends Component{
     constructor(props){
         super(props);
-        this.props.pagination.total = this.props.data.length;
     }
 
-    pagination_pre(){
-        console.log('page pre');
-        this.props.pagination.index--;
-    }
+    pagination_pre = ()=>{
+        this.props.onHandlePagePre();
+    };
 
-    pagination_next(){
-        console.log('page next');
-        this.props.pagination.index++;
-        console.log(this.props.pagination);
-    }
+    pagination_next = ()=>{
+        this.props.onHandlePageNext();
+    };
 
     render(){
+        const total = filterList(this.props.data,this.props.search).length;
         return (
             <div className="pagination">
-                共有<span>{this.props.pagination.total}</span>项,
-                当前显示第<span>{this.props.pagination.index}</span>/<span>{Math.ceil(this.props.pagination.total/this.props.pagination.per)}</span>页
+                共有<span>{total}</span>项,
+                每页显示<span>{this.props.pagination.per}</span>项,
+                当前显示第<span>{total===0?0:this.props.pagination.index}</span>/<span>{Math.ceil(total/this.props.pagination.per)}</span>页
                 &nbsp;&nbsp;&nbsp;&nbsp;
-                <button onClick={()=>this.pagination_pre()} disabled={this.props.pagination.index==1}>上一页</button>
-                <button onClick={()=>this.pagination_next()} disabled={this.props.pagination.index==Math.ceil(this.props.pagination.total/this.props.pagination.per)}>下一页</button>
+                <button onClick={this.pagination_pre} disabled={this.props.pagination.index<=1}>上一页</button>
+                <button onClick={this.pagination_next} disabled={this.props.pagination.index>=Math.ceil(total/this.props.pagination.per)}>下一页</button>
             </div>
         );
     }
 }
 
 class ResultCap extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    handleSort = (item,arr)=>{
+        this.props.onHandleSort(item,arr);
+    };
+
     render(){
         return (
             <div className="ResultCap">
                 <span>序号</span>
                 <span><input type="checkbox"/>全选</span>
-                <span>item1</span>
-                <span>item2</span>
-                <span>item3</span>
+                <span>姓名</span>
+                <span>年龄
+                    <i onClick={()=>{this.handleSort('age','up')}} style={{cursor: "pointer"}}>&nbsp;↑&nbsp;</i>&nbsp;&nbsp;
+                    <i onClick={()=>{this.handleSort('age','down')}} style={{cursor: "pointer"}}>&nbsp;↓&nbsp;</i>
+                </span>
+                <span>年级
+                    <i onClick={()=>{this.handleSort('grade','up')}} style={{cursor: "pointer"}}>&nbsp;↑&nbsp;</i>&nbsp;&nbsp;
+                    <i onClick={()=>{this.handleSort('grade','down')}} style={{cursor: "pointer"}}>&nbsp;↓&nbsp;</i>
+                </span>
                 <span>操作</span>
             </div>
         );
@@ -142,6 +130,14 @@ class ResultCap extends Component{
 }
 
 class ResultItem extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    handleItemDelete = (item)=>{
+        this.props.onHandleItemDelete(item);
+    };
+
     render(){
         return (
             <div className="ResultItem">
@@ -150,7 +146,10 @@ class ResultItem extends Component{
                 <span>{this.props.item.name}</span>
                 <span>{this.props.item.age}</span>
                 <span>{this.props.item.grade}</span>
-                <span><button>delete</button></span>
+                <span>
+                    <button onClick={()=>{this.handleItemDelete(this.props.item)}}>delete</button>&nbsp;&nbsp;
+                    <button onClick={()=>{this.handleItemDelete(this.props.item)}}>edit</button>&nbsp;&nbsp;
+                </span>
             </div>
         );
     }
@@ -159,23 +158,49 @@ class ResultItem extends Component{
 class ResultContent extends Component{
     constructor(props){
         super(props);
-        console.log(this.props);
     }
+
+    handleSort = (item,arr)=>{
+        this.props.onHandleSort(item,arr);
+    };
+
+    handleItemDelete = (item)=>{
+        this.props.onHandleItemDetele(item);
+    };
+
     render(){
-        var rows = [];
-        // this.props.data.map((item,index)=>{
-        //    rows.push(<ResultItem item={item} index={index} key={item.name}></ResultItem>);
-        // });
-        rows = pagination(this.props.data,this.props.pagination.index,this.props.pagination.per);
-        return (
+        const list = filterList(this.props.data,this.props.search);
+        // const rows = pagination(this.props.data,this.props.pagination.index,this.props.pagination.per);
+        const rows = pagination(list,this.props.pagination.index,this.props.pagination.per,this.handleItemDelete);
+        if(rows.length==0){
+            return (
+                <div className="ResultContent">
+                    <ResultCap onHandleSort={this.handleSort}></ResultCap>
+                    <div className="ResultList">
+                        无数据
+                    </div>
+                </div>
+            );
+        }else{
+            return (
+                <div className="ResultContent">
+                    <ResultCap onHandleSort={this.handleSort}></ResultCap>
+                    <div className="ResultList">
+                        {rows}
+                    </div>
+                </div>
+            );
+        }
+
+        /*return (
             <div className="ResultContent">
-                <ResultCap></ResultCap>
+                <ResultCap onHandleSort={this.handleSort}></ResultCap>
                 <div className="ResultList">
                     {rows}
                 </div>
             </div>
 
-        );
+        );*/
     }
 }
 
@@ -190,23 +215,89 @@ class List extends Component{
             },
             pagination: {
                 index: 1,
-                total: 0,
+                //total: 0,
                 //total: this.props.students.length,
-                per: 5
-            }
+                per: 3
+            },
+            realList: this.props.students
         };
     }
+
+    handlePagePre = ()=>{
+        this.setState((prevState) =>{
+            pagination:{
+                index: prevState.pagination.index--
+            }
+        });
+    };
+
+    handlePageNext = ()=>{
+        this.setState((prevState)=>{
+            pagination:{
+                index: prevState.pagination.index++
+            }
+        })
+    };
+
+    handleSearchName = (name)=>{
+        this.setState({
+            search:{
+                name: name
+            }
+        })
+    };
+
+    handleSort = (item,arr)=>{
+        // console.log(item+'//'+arr);
+        // if(item==='age'){
+        //     this.setState((prevState)=>{
+        //         realList:prevState.realList.sort((a, b) => {
+        //             return arr==='up'?(a['age']-b['age']):(b['age']-a['age']);
+        //         })
+        //     })
+        // }
+
+        this.setState((prevState)=>{
+            realList:prevState.realList.sort((a, b) => {
+                return arr==='up'?(a[item]-b[item]):(b[item]-a[item]);
+            })
+        })
+    };
+
+    handleItemDelete = (item)=>{
+        this.setState((preState)=>{
+            preState.realList.splice(preState.realList.indexOf(item),1);
+            realList: preState.realList;
+            if(preState.realList.length%preState.pagination.per == 0){
+                pagination:{
+                    index: preState.pagination.index==0?preState.pagination.index:preState.pagination.index--
+                }
+            }
+        })
+    };
+
     render(){
+
         return(
             <div>
-                <Search search={this.state.search}/>
+                <Search
+                    search={this.state.search}
+                    onHandleSearchName={this.handleSearchName}
+                />
                 <ResultContent
-                    data={this.props.students}
+                    // data={this.props.students}
+                    data = {this.state.realList}
+                    search={this.state.search}
                     pagination={this.state.pagination}
-                    search={this.state.search} />
+                    onHandleSort={this.handleSort}
+                    onHandleItemDetele={this.handleItemDelete}/>
                 <Pagination
-                    data={this.props.students}
-                    pagination={this.state.pagination} />
+                    //data={this.props.students}
+                    data = {this.state.realList}
+                    search={this.state.search}
+                    pagination={this.state.pagination}
+                    onHandlePagePre={this.handlePagePre}
+                    onHandlePageNext={this.handlePageNext}/>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -7,46 +7,53 @@ import './index.css';
 
 var data = [
     {
-        name: 'John1',
+        name: 'Amy',
         age: 37,
         grade: 1
     },
     {
-        name: 'John2',
+        name: 'John',
         age: 19,
         grade: 2
     },
     {
-        name: 'John3',
+        name: 'Stuff',
         age: 25,
         grade: 3
     },
     {
-        name: 'John4',
+        name: 'Hoke',
         age: 11,
         grade: 4
     },
     {
-        name: 'John5',
+        name: 'Tom',
         age: 44,
         grade: 5
     },
     {
-        name: 'John6',
+        name: 'Johny',
         age: 27,
         grade: 6
     },
     {
-        name: 'John7',
+        name: 'Alis',
         age: 29,
         grade: 7
     },
     {
-        name: 'John8',
+        name: 'Tomas',
+        age: 41,
+        grade: 4
+    },
+    {
+        name: '李燕',
         age: 41,
         grade: 4
     }
 ];
+
+var empty = [];
 
 ReactDOM.render(<List students={data} />, document.getElementById('root'));
 registerServiceWorker();
